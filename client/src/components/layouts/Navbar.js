167ce05706f8f1ce67cbe28bc69c28b3569fb03e
malekitoon <malekitoon@yahoo.com>
2020-09-17 22:08:56 +0300

    046 - Starting On The Dashboard

--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -12,10 +12,21 @@ const Navbar = ({ logout, auth: { isAuthenticated, loading } }) => {
   const authLinks = (
     <ul>
       <li>
+        <Link to='/dashboard'>
+          <i className='fas fa-user' />
+          <span className='hide-sm'>
+            {' '}
+            Dashboard
+          </span>
+        </Link>
+      </li>
+      <li>
         <a href='#!' onClick={logout}>
           <i className='fas fa-sign-out-alt' />
-          {' '}
-          <span className='hide-sm'>Logout</span>
+          <span className='hide-sm'>
+            {' '}
+            Logout
+          </span>
         </a>
       </li>
     </ul>
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,5 +1,6 @@
 import {
   GET_PROFILE,
+  CLEAR_PROFILE,
   PROFILE_ERROR,
 } from '../actions/types';
 
@@ -17,6 +18,8 @@ export default function (state = initialState, { type, payload }) {
       return { ...state, profile: payload, loading: false };
     case PROFILE_ERROR:
       return { ...state, error: payload, loading: false };
+    case CLEAR_PROFILE:
+      return { ...state, profile: null, repos: [], loading: false };
     default:
       return state;
   }
