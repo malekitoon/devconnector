8e8f69922694525a7ef9ed81cc25e266dfe1f41b
malekitoon <malekitoon@yahoo.com>
2020-09-18 23:51:10 +0300

    050 - Add Education Experiences

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import {
   GET_PROFILE,
   PROFILE_ERROR,
+  UPDATE_PROFILE,
 } from './types';
 import { setAlert } from './alert';
 
@@ -42,6 +43,68 @@ export const createProfile = (formData, history, edit = false) => async dispatch
     if (!edit) {
       history.push('/dashboard');
     }
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: err.response.statusText,
+        status: err.response.status,
+      },
+    });
+
+    const { errors } = err.response.data;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+  }
+};
+
+// Add Experience
+export const addExperience = (formData, history) => async dispatch => {
+  try {
+    const config = { headers: { 'Content-Type': 'application/json' } };
+
+    const res = await axios.put('/api/profile/experience', formData, config);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data,
+    });
+
+    dispatch(setAlert('Experience Added', 'success'));
+    history.push('/dashboard');
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: err.response.statusText,
+        status: err.response.status,
+      },
+    });
+
+    const { errors } = err.response.data;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+  }
+};
+
+// Add Education
+export const addEducation = (formData, history) => async dispatch => {
+  try {
+    const config = { headers: { 'Content-Type': 'application/json' } };
+
+    const res = await axios.put('/api/profile/education', formData, config);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data,
+    });
+
+    dispatch(setAlert('Education Added', 'success'));
+    history.push('/dashboard');
   } catch (err) {
     dispatch({
       type: PROFILE_ERROR,
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -69,7 +69,10 @@ router.post('/', [ auth, [
     if(bio) profileFields.bio = bio;
     if(status) profileFields.status = status;
     if(githubusername) profileFields.githubusername = githubusername;
-    if(skills) profileFields.skills = skills.split(',').map(skill => skill.trim());
+    if(skills) profileFields.skills = (
+      typeof skills === 'String'
+      ? skills.split(',')
+      : skills).map(skill => skill.trim());
 
     // Build social object
     profileFields.social = {};
