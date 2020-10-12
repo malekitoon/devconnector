531e526a9c295362983176d8ead1a808fd19ab1a
malekitoon <malekitoon@yahoo.com>
2020-10-12 12:49:46 +0300

    053 Finish Profile Actions  Reducer

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,8 +1,10 @@
 import {
   GET_PROFILE,
+  GET_PROFILES,
   CLEAR_PROFILE,
   PROFILE_ERROR,
   UPDATE_PROFILE,
+  GET_REPOS,
 } from '../actions/types';
 
 const initialState = {
@@ -18,10 +20,14 @@ export default function (state = initialState, { type, payload }) {
     case GET_PROFILE:
     case UPDATE_PROFILE:
       return { ...state, profile: payload, loading: false };
+    case GET_PROFILES:
+      return { ...state, profiles: payload, loading: false };
     case PROFILE_ERROR:
       return { ...state, error: payload, loading: false };
     case CLEAR_PROFILE:
       return { ...state, profile: null, repos: [], loading: false };
+    case GET_REPOS:
+      return { ...state, repos: payload, loading: false };
     default:
       return state;
   }
